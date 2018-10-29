e5f3bd16aaa7eef23b9f99c05f9a0e1c043eecba
daixx271 <daixx271@umn.edu>
2018-10-29 17:34:38 -0400

    update test file

--- First Iteration/test.py
+++ First Iteration/test.py
@@ -16,15 +16,35 @@ def test_chessboard():
         assert (t3,1)
         t4 = chessboard.changevalue(2,3,1)
         assert (t4,0)
-
-        #black win
+        #black win 1 (five in a row)
         for n in range(5):
-                b_valid = chessboard.changevalue(0,n,1) # put black at first row in order
-                assert (b_valid,1)
-                w_valid = chessboard.changevalue(random.randint(1,15),2*n,2) # put black at first row in order
-                assert (w_valid,1)
-        chessboard.checkwinner()
-
-
-
-        
+                b_valid1 = chessboard.changevalue(0,n,1)
+                assert (b_valid1,1)
+                w_valid1 = chessboard.changevalue(random.randint(1,15),random.randint(5,15),2)
+                assert (w_valid1,1)
+        winner1 = chessboard.checkwinner()
+        assert (winner1,1)
+        #black win 2 (five in a column)
+        for n in range(5):
+                b_valid2 = chessboard.changevalue(n,0,1)
+                assert (b_valid2,1)
+                w_valid2 = chessboard.changevalue(random.randint(5,15),random.randint(1,15),2)
+                assert (w_valid2,1)
+        winner2 = chessboard.checkwinner()
+        assert (winner2,1)
+        #black win 3 (five in a polyline)
+        for n in range(5):
+                b_valid3 = chessboard.changevalue(n,n,1)
+                assert (b_valid3,1)
+                w_valid3 = chessboard.changevalue(random.randint(5,15),random.randint(5,15),2)
+                assert (w_valid3,1)
+        winner3 = chessboard.checkwinner()
+        assert (winner3,1)
+        #white win 1
+        for n in range(5):
+                b_valid4 = chessboard.changevalue(5,2*n,1)
+                assert (b_valid4,1)
+                w_valid4 = chessboard.changevalue(3,n,2)
+                assert (w_valid4,1)
+        winner4 = chessboard.checkwinner()
+        assert (winner4,2)
