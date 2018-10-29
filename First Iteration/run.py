8c252cf6b945347781be841ad9a82844fe8ec025
Yiqian Wang <yiqianwang@dyn-160-39-184-181.dyn.columbia.edu>
2018-10-29 17:29:46 -0400

    update

--- First Iteration/run.py
+++ First Iteration/run.py
@@ -8,24 +8,13 @@ Created on Mon Oct 29 13:45:19 2018
 Main function
 """
 
-from showchessboard3 import *
-from login import *
-import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QMessageBox
-from PyQt5.QtGui import QPixmap, QIcon, QFont
-from PyQt5.QtCore import Qt
-from chessboard import chessboard as CB
+from showchessboard4 import *
 
 def main():
     app = QApplication(sys.argv)
-
-    root = Tk()
-    lf = LoginFrame(root)
-    root.mainloop()
-
-    # game = Gomoku()
-    # game.show()
-    # sys.exit(app.exec_())
-    
+    game = Gomoku()
+    game.show()
+    sys.exit(app.exec_())
+        
 if __name__ == '__main__':
     main()
