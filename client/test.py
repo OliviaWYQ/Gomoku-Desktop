b31085072453c5f2c607bda6cc35b4213835d4b7
Zhongkai Sun <zksun95@gmail.com>
2018-10-30 21:42:14 -0400

    pytest

--- client/test.py
+++ client/test.py
@@ -17,40 +17,42 @@ def test_chessboard():
         assert t3==1
         t4 = obj.changevalue(2,3,1)
         assert t4==0
-        #black win 1 (five in a row)
         obj.reset()
+        #black win 1 (five in a row)
         for n in range(5):
                 b_valid1 = obj.changevalue(0,n,1)
                 assert b_valid1==1
-                w_valid1 = obj.changevalue(3,2*n,2)
+                w_valid1 = obj.changevalue(2,2*n,2)
                 assert w_valid1==1
         winner1 = obj.checkwinner()
         assert winner1==1
-        #black win 2 (five in a column)
-
         obj.reset()
+        #black win 2 (five in a column)
         for n in range(5):
                 b_valid2 = obj.changevalue(n,0,1)
                 assert b_valid2==1
-                w_valid2 = obj.changevalue(2*n,10,2)
+                w_valid2 = obj.changevalue(2*n,3,2)
                 assert w_valid2==1
         winner2 = obj.checkwinner()
         assert winner2==1
-        #black win 3 (five in a polyline)
         obj.reset()
+        #black win 3 (five in a polyline)
         for n in range(5):
                 b_valid3 = obj.changevalue(n,n,1)
                 assert b_valid3==1
-                w_valid3 = obj.changevalue(8+n,5+2*n,2)
+                w_valid3 = obj.changevalue(5,2*n,2)
                 assert w_valid3==1
         winner3 = obj.checkwinner()
         assert winner3==1
-        #white win 1
         obj.reset()
+        #white win 1
         for n in range(5):
                 b_valid4 = obj.changevalue(5,2*n,1)
                 assert b_valid4==1
                 w_valid4 = obj.changevalue(3,n,2)
                 assert w_valid4==1
         winner4 = obj.checkwinner()
-        assert winner4==2
+        assert winner4 == 2
+        obj.reset()
+
+test_chessboard()
